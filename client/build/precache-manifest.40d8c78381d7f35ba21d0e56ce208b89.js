d36e02f17273240d3a2e8a0d2c04ef5908a7dc75
jacob-af <jfeitler@gmail.com>
2024-02-17 22:25:41 -0500

    build

---
+++ client/build/precache-manifest.40d8c78381d7f35ba21d0e56ce208b89.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7dc7986d0f886864afc329c390b4278",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9623ef6887718c6bc179",
+    "url": "/static/css/2.43b57041.chunk.css"
+  },
+  {
+    "revision": "4bd3490b6bf2d8db433e",
+    "url": "/static/css/main.392158d7.chunk.css"
+  },
+  {
+    "revision": "9623ef6887718c6bc179",
+    "url": "/static/js/2.435f0c55.chunk.js"
+  },
+  {
+    "revision": "63dbbc77a13c0632257719392f089b15",
+    "url": "/static/js/2.435f0c55.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4bd3490b6bf2d8db433e",
+    "url": "/static/js/main.03ed7434.chunk.js"
+  },
+  {
+    "revision": "d08766e1dd2bf4f6c209",
+    "url": "/static/js/runtime-main.992eac29.js"
+  },
+  {
+    "revision": "0edea1d33bb52870f9dbe3e5688e8db1",
+    "url": "/static/media/Toast.0edea1d3.png"
+  },
+  {
+    "revision": "3cdbd1c0049f68c38a2970b5afcc7877",
+    "url": "/static/media/angrykitty.3cdbd1c0.mp3"
+  },
+  {
+    "revision": "157f990cfba4fc1ca58c1ece94f1d69c",
+    "url": "/static/media/cathiss.157f990c.mp3"
+  },
+  {
+    "revision": "3c78ce4c7fa1fa428e9e2f70b5d3994a",
+    "url": "/static/media/corgi.3c78ce4c.png"
+  },
+  {
+    "revision": "17dd8e50c40f07df8998523867e64d15",
+    "url": "/static/media/dogbark.17dd8e50.mp3"
+  },
+  {
+    "revision": "61e2338da118947bf326c700c2e6d11b",
+    "url": "/static/media/dogbarkgrowl.61e2338d.mp3"
+  },
+  {
+    "revision": "0518424637c04161da46b525a4548cca",
+    "url": "/static/media/glowykitty.05184246.png"
+  },
+  {
+    "revision": "d156bb07e9f6c5e3293f154735622262",
+    "url": "/static/media/grass_field.d156bb07.png"
+  },
+  {
+    "revision": "53ab6bd567abb64df0868d6778ce1eed",
+    "url": "/static/media/great_dane.53ab6bd5.png"
+  },
+  {
+    "revision": "d605c05704fd517aa12032282a972fb4",
+    "url": "/static/media/green-check.d605c057.png"
+  },
+  {
+    "revision": "d55071d6d263bf799b2b15648fdd5a16",
+    "url": "/static/media/kittenmeow.d55071d6.mp3"
+  },
+  {
+    "revision": "d115ffba975a4866d06b1d19c569164b",
+    "url": "/static/media/mainecoon.d115ffba.png"
+  },
+  {
+    "revision": "e3a578dbcc22f70281f97434c26afe94",
+    "url": "/static/media/munchkin.e3a578db.png"
+  },
+  {
+    "revision": "21b71622763a4fee61e3c2b2e9b25d6c",
+    "url": "/static/media/red-x.21b71622.png"
+  },
+  {
+    "revision": "f8140b5789fbd5386c68bc742f6cf0cd",
+    "url": "/static/media/sammie.f8140b57.png"
+  },
+  {
+    "revision": "16a4dfbcb257dbbe50564eb035291544",
+    "url": "/static/media/savannahcat.16a4dfbc.png"
+  },
+  {
+    "revision": "36ed09eac7928e8a48dfe5c356e227cf",
+    "url": "/static/media/sheeb.36ed09ea.png"
+  },
+  {
+    "revision": "6c090cd7507472e9431ec5cd6998611e",
+    "url": "/static/media/squeakytoy.6c090cd7.mp3"
+  },
+  {
+    "revision": "aad5600c1289a5d18f8756f24e18f25f",
+    "url": "/static/media/tuxedokitty.aad5600c.png"
+  }
+]);
